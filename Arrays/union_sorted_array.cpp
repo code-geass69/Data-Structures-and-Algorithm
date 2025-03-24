a3c41a34e925f1fb32ea11fad724bdc627226cdf
Atharv More <atharvmore30@gmail.com>
2025-03-24 08:35:32 +0530

    modified:   Arrays/union_sorted_array.cpp

--- Arrays/union_sorted_array.cpp
+++ Arrays/union_sorted_array.cpp
@@ -4,23 +4,58 @@
 using namespace std;
 
 
-void arr_union(vector<int>& arr1, vector<int>& arr2, int num1, int num2){
-    set<int> st;
-    for (int i = 0; i < num1; i++)
-    {
-        st.insert(arr1[i]);
-    }
-    for (int i = 0; i < num2; i++)
-    {
-        st.insert(arr2[i]);
-    }
-    vector<int> temp;
-    for(auto it : st){
-        temp.push_back(it);
-    }
+// void arr_union(vector<int>& arr1, vector<int>& arr2, int num1, int num2){
+//     set<int> st;
+//     for (int i = 0; i < num1; i++)
+//     {
+//         st.insert(arr1[i]);
+//     }
+//     for (int i = 0; i < num2; i++)
+//     {
+//         st.insert(arr2[i]);
+//     }
+//     vector<int> temp;
+//     for(auto it : st){
+//         temp.push_back(it);
+//     }
     
 
-        
+//Optimal       
+void arr_union(vector<int>& arr1, vector<int>& arr2, int num1, int num2) {
+    int i = 0, j = 0;
+    vector<int> unionArr;
+
+    while (i < num1 && j < num2) {
+        if (arr1[i] <= arr2[j]) {
+            if (unionArr.empty() || unionArr.back() != arr1[i]) {
+                unionArr.push_back(arr1[i]);
+            }
+            i++;
+        } else {
+            if (unionArr.empty() || unionArr.back() != arr2[j]) {
+                unionArr.push_back(arr2[j]);
+            }
+            j++;
+        }
+    }
+
+    while (i < num1) {  // Fix: Increment i to avoid infinite loop
+        if (unionArr.empty() || unionArr.back() != arr1[i]) {
+            unionArr.push_back(arr1[i]);
+        }
+        i++;  // Fix: Increment i
+    }
+
+    while (j < num2) {
+        if (unionArr.empty() || unionArr.back() != arr2[j]) {
+            unionArr.push_back(arr2[j]);
+        }
+        j++;
+    }
+
+    for (int x : unionArr) {
+        cout << x << " ";
+    }
 }
 
 int main()
